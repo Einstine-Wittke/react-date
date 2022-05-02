9350f75adf9ca35c5ce38c2714e2ea1997341fbe
Einstine-Wittke <wittkeeinstine@gmail.com>
2022-05-03 00:52:24 +0530

    netflix

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
+import { logDOM } from '@testing-library/react'
 import React from 'react'
 import './App.css'
+import Props from './PROPS/Props'
+import SeriesData from './PROPS/SeriesData'
 
 
 const App = () => {
-
-
-  let curDate = new Date();
-  curDate = curDate.getHours();
-
-  let greeting=''
-
-  const style = {};
-
-  if (curDate >=1 && curDate<12) {
-    greeting="Buenos Dias ! "
-    style.color="green"
-  } else if (curDate >=12  && curDate <19){
-    greeting="Buenas Tardes !"
-    style.color="orange"
-  } else{
-    greeting ="Buenas Noches ! "
-    style.color="grey"
-  }
-
   return (
     <div className='App'>
-  <h1> Hola Amigos ! , <span style={style}> {greeting} </span> </h1>
+      <h1>Top 5 Netflix Series of 2022</h1>
+
+      {SeriesData.map((value,index)=> {
+        console.log(index)
+        return (
+          <Props key={index}
+            image={value.image}
+            title={value.title}
+            name={value.name}
+            link={value.link} />
+        )
+      })}
     </div>
   )
 }
